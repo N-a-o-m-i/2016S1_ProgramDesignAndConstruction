d1c879ee3f135270576ad7106297ed6aa1789c4a
Naooomi <naomi.wuuu@gmail.com>
2016-05-27 22:58:58 +1200

    PDC_Lab6_JDBCLab

--- PDC_Week6_JDBCLab/src/bookstore/BookStore.java
+++ PDC_Week6_JDBCLab/src/bookstore/BookStore.java
@@ -1,21 +1,47 @@
 package bookstore;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class BookStore {
+public class BookStore
+{
 
     Connection conn=null;
-    String url="";  //url of the DB host
-    String username="";  //your DB username
-    String password="";   //your DB password
-
-    public void connectBookStoreDB()
+    //String url="jdbc:derby:BookStoreDB;create=true";  
+    String url = "jdbc:derby://localhost:1527/BookStroeDB"; //url of the DB host
+    String username="PDC";  //your DB username
+    String password="PDC";   //your DB password
+    Statement statement;
+    ResultSet rs;
+    
+    public void autoConnectBookStoreDB()
     {
+        try
+        {
+            conn = DriverManager.getConnection(url, username, password);
+            statement = conn.createStatement();
+            checkTableExisting("BOOK");
+            statement.executeUpdate("CREATE TABLE BOOK (BOOKID INT, TITLE VARCHAR(50), CATEGORY VARCHAR(20), PRICE FLOAT"); //Create BOOK table
+            statement.executeUpdate("INSERT INTO BOOK VALUES (1,'Slum Dog Millionaire','Fiction',19.90)," //Insert Values into BOOK table
+                                + "(2,'Run Mummy Run','Fiction',28.00),"
+                                + "(3,'The Land of Painted Caves','Fiction',15.40),"
+                                + "(4,'Cook with Jamie','Non-fiction',55.20),"
+                                + "(5,'Far Eastern Odyssey','Non-fiction',24.90),"
+                                + "(6,'Open','Non-fiction',33.60),"
+                                + "(7,'Big Java', 'Textbook',55.00),"
+                                + "(8,'Financial Accounting','Textbook',24.80);");
+            
+        }
+        catch(Throwable e)
+        {
+            System.err.println("SQL Exception: " + e.getMessage());
+        } 
     }
 
 
@@ -34,7 +60,6 @@ public class BookStore {
       */
 
     }
-
 
     public ResultSet getWeekSpecial()
     {
@@ -66,6 +91,37 @@ public class BookStore {
         }
     }
 
+    private void checkTableExisting(String newTableName)
+    {
+        try
+        {
+            System.out.println("check existing tables...");
+            String[] types = {"TABLE"};
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet rsDBMeta = dbmd.getTables(null, null, null, null); //types
+            Statement dropStatement = null;
+            
+            while(rsDBMeta.next())
+            {
+                String tableName = rsDBMeta.getString("TABLE_NAME");
+                System.out.println("found: " + tableName);
+                if(tableName.compareToIgnoreCase(newTableName) == 0)
+                {
+                    System.out.println(tableName + " needs to be deleted");
+                    String sqlDropTable = "DROP TABLE " + newTableName;
+                    
+                    
+                    
+                }
+                
+                
+            }
+            
+        }
+        catch()
+        {
+        }
+    }
 
 
 }
